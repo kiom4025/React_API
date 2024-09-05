460e9fd3c254ce06549c078904c2f50be63d7a35
kiom_ <moki4994@gmail.com>
2024-09-05 12:51:31 +0530

    Nodemon actived in package.json, Notes added for HTTP status code, setup a first router and viewed the output of both get and post request

---
+++ Reference/Notes.js
@@ -0,0 +1,4 @@
+const reference ={
+    statusCode : "https://www.geeksforgeeks.org/what-are-http-status-codes/",
+    
+}
---
+++ routes/firstRouter.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',(req, res)=>{
+    // 1- To return a string
+    // res.send("post method called")
+    
+    // 2- To return an array
+    res.json({
+        messageTitle:"Hi",
+        message:"get method called"
+    })
+})
+
+router.post("/create", (req,res)=>{
+    // 1- To return a string
+    res.send("post method called")
+    
+    // 2- To return an array
+    // res.json({
+    //     messageTitle:"Hi",
+    //     message:"Post method called"
+    // })
+})
+
+module.exports = router;
